254a158563d41fc35dc85def8aee58564cbe63db
freedomlang <blackywkl@gmail.com>
2016-03-04 11:34:51 +0800

    remove some settings

--- src/jquery.goup.js
+++ src/jquery.goup.js
@@ -7,219 +7,149 @@
  * Version 1.0.0
  *
  */
-(function ( $ ) {
-	
-	$.goup = function(user_params) {
-		
-		/* Default Params */
-		var params = $.extend({
-				location : 'right',
-				locationOffset : 20,
-				bottomOffset : 10,
-				containerSize : 40,
-				containerRadius : 10,
-				containerClass : 'goup-container',
-				arrowClass : 'goup-arrow',
-				alwaysVisible : false,
-				trigger: 500,
-				entryAnimation : 'fade',
-				goupSpeed : 'slow',
-				hideUnderWidth : 500,
-				containerColor : '#000',
-				arrowColor : '#fff',
-                title : '',
-                titleAsText : false,
-                titleAsTextClass : 'goup-text'
-			}, user_params);
-		/* */
-		
-		
-		$('body').append('<div style="display:none" class="'+params.containerClass+'"></div>');
-		var container = $('.'+params.containerClass);
-		$(container).html('<div class="'+params.arrowClass+'"></div>');
-		var arrow = $('.'+params.arrowClass);
-		
-		/* Parameters check */
-		if (params.location != 'right' && params.location != 'left') {
-			params.location = 'right';
-		}
-		
-		if (params.locationOffset < 0) {
-			params.locationOffset = 0;
-		}
-		
-		if (params.bottomOffset < 0) {
-			params.bottomOffset = 0;
-		}
-		
-		if (params.containerSize < 20) {
-			params.containerSize = 20;
-		}
-
-		if (params.containerRadius < 0) {
-			params.containerRadius = 0;
-		}
-		
-		if (params.trigger < 0) {
-			params.trigger = 0;
-		}
-		
-		if (params.hideUnderWidth < 0) {
-			params.hideUnderWidth = 0;
-		}
-		
-		var checkColor = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i;
-		if (!checkColor.test(params.containerColor)) {
-			params.containerColor = '#000';
-		}
-		if (!checkColor.test(params.arrowColor)) {
-			params.arrowColor = '#fff';
-		}
-        
-        if (params.title === '') {
-            params.titleAsText = false;
-        }
-		/* */
-		
-		/* Container Style */
-		var containerStyle = {};
-		containerStyle = {
-			position : 'fixed',
-			width : params.containerSize,
-			height : params.containerSize,
-			background : params.containerColor,
-			cursor: 'pointer'
-		};
-		containerStyle['bottom'] = params.bottomOffset;
-		containerStyle[params.location] = params.locationOffset;
-		containerStyle['border-radius'] = params.containerRadius;
-		
-		$(container).css(containerStyle);
-        if (!params.titleAsText) {
-            $(container).attr('title', params.title);
+(function(e) {
+    function t(e, t, n) {
+        if (t == "show") {
+            switch (n) {
+            case "fade":
+                e.fadeIn();
+                break;
+            case "slide":
+                e.slideDown();
+                break;
+            default:
+                e.fadeIn()
+            }
         } else {
-            $('body').append('<div class="'+params.titleAsTextClass+'">'+params.title+'</div>');
-            var textContainer = $('.'+params.titleAsTextClass);
-            $(textContainer).attr('style', $(container).attr('style'));
-            $(textContainer).css('background', 'transparent')
-                           .css('width', params.containerSize + 40)
-                           .css('height', 'auto')
-                           .css('text-align', 'center')
-                           .css(params.location, params.locationOffset - 20);
-            var containerNewBottom = $(textContainer).height() + 10;
-            $(container).css('bottom', '+='+containerNewBottom+'px');
+            switch (n) {
+            case "fade":
+                e.fadeOut();
+                break;
+            case "slide":
+                e.slideUp();
+                break;
+            default:
+                e.fadeOut()
+            }
         }
-            
-		
-		/* Arrow Style */		
-		var arrowStyle = {};
-		var borderSize = 0.25 * params.containerSize;
-		arrowStyle = {
-			width : 0,
-			height : 0,
-			margin : '0 auto',
-			'padding-top' : Math.ceil(0.325 * params.containerSize),
-			'border-style' : 'solid',
-			'border-width' : '0 '+borderSize+'px '+borderSize+'px '+borderSize+'px',
-			'border-color' : 'transparent transparent '+params.arrowColor+' transparent' 
-		};
-		$(arrow).css(arrowStyle);
-		/* */
-		
-		
-		
-		/* params.trigger Hide under a certain width */
-		var isHidden = false;
-		$(window).resize(function(){
-			if ($(window).outerWidth() <= params.hideUnderWidth) {
-				isHidden = true;
-				do_animation($(container), 'hide', params.entryAnimation);
-                if (textContainer)
-                    do_animation($(textContainer), 'hide', params.entryAnimation);
-			} else {
-				isHidden = false;
-				$(window).trigger('scroll');
-			}
-		});
-		/* If i load the page under a certain width, i don't have the event 'resize' */
-		if ($(window).outerWidth() <= params.hideUnderWidth) {
-			isHidden = true;
-			$(container).hide();
-            if (textContainer)
-                $(textContainer).hide();
-		}
-		
-		
-		/* params.trigger show event */
-		if (!params.alwaysVisible) {
-			$(window).scroll(function(){
-				if ($(window).scrollTop() >= params.trigger && !isHidden) {
-					do_animation($(container), 'show', params.entryAnimation);
-                    if (textContainer)
-                        do_animation($(textContainer), 'show', params.entryAnimation);
-				}
-				
-				if ($(window).scrollTop() < params.trigger && !isHidden) {
-					do_animation($(container), 'hide', params.entryAnimation);
-                    if (textContainer)
-                        do_animation($(textContainer), 'hide', params.entryAnimation);
-				}
-			});
-		} else {
-			do_animation($(container), 'show', params.entryAnimation);
-            if (textContainer)
-                do_animation($(textContainer), 'show', params.entryAnimation);
-		}
-		/* If i load the page and the scroll is over the trigger, i don't have immediately the event 'scroll' */
-		if ($(window).scrollTop() >= params.trigger && !isHidden) {
-			do_animation($(container), 'show', params.entryAnimation);
-            if (textContainer)
-                do_animation($(textContainer), 'show', params.entryAnimation);
-		}
-		
-		/* Click event */
-		var notClicked = true;
-		$(container).add(textContainer).on('click', function(){
-			if (notClicked) {
-				notClicked = false;
-				$('html,body').animate({ scrollTop: 0 }, params.goupSpeed, function(){ notClicked = true });
-			}
-			return false;
-
-		});		
-        
-	};
-	
-	
-	/* Private function for the animation */
-	function do_animation(obj, type, animation) {
-		if (type == 'show') {
-			switch(animation) {
-				case 'fade':
-					obj.fadeIn();
-				break;
-				
-				case 'slide':
-					obj.slideDown();
-				break;
-				
-				default:
-					obj.fadeIn();
-			}
-		} else {
-			switch(animation) {
-				case 'fade':
-					obj.fadeOut();
-				break;
-				
-				case 'slide':
-					obj.slideUp();
-				break;
-				
-				default:
-					obj.fadeOut();
-			}
-		}
-	}
-	
-}( jQuery ));
+    }
+    e.goup = function(n) {
+        var r = e.extend({
+            location: "right",
+            locationOffset: 150,
+            bottomOffset: 100,
+            containerSize: 40,
+            containerRadius: 10,
+            containerClass: "goup-container",
+            alwaysVisible: false,
+            trigger: 250,
+            entryAnimation: "fade",
+            goupSpeed: "slow",
+            hideUnderWidth: 500,
+            title: "",
+            titleAsText: false,
+            titleAsTextClass: "goup-text",
+            img: "http://freedomlang.com/usr/themes/default/img/arrow_up.svg"
+        }, n);
+        e("body").append('<svg fill="#eee" ' + 'style="display:none" class="' + r.containerClass + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="36.1 192 576 372" enable-background="new 36.1 192 576 372" xml:space="preserve"><g><g transform="translate(1.000000, 2.000000)"><path d="M563,406c-7.2,0-14.5-1.6-21.5-5.1L323.1,291.7L104.5,400.9c-23.8,11.9-52.5,2.2-64.4-21.5    c-11.9-23.7-2.2-52.5,21.5-64.4l240-120c13.5-6.8,29.4-6.8,42.9,0l240,120c23.7,11.8,33.3,40.7,21.5,64.4    C597.6,396.3,580.6,406,563,406L563,406z"></path><path d="M563,562c-3.6,0-7.2-0.8-10.7-2.5L323.1,444.8L93.8,559.5c-11.8,5.9-26.3,1.1-32.2-10.7    c-5.9-11.8-1.1-26.2,10.8-32.2l240-120c6.8-3.4,14.7-3.4,21.5,0l240,120c11.9,5.9,16.7,20.3,10.8,32.2    C580.3,557.1,571.8,562,563,562L563,562z"></path></g></g></svg>');
+        var i = e("." + r.containerClass);
+        if (r.location != "right" && r.location != "left") {
+            r.location = "right"
+        }
+        if (r.locationOffset < 0) {
+            r.locationOffset = 0
+        }
+        if (r.bottomOffset < 0) {
+            r.bottomOffset = 0
+        }
+        if (r.containerSize < 20) {
+            r.containerSize = 20
+        }
+        if (r.containerRadius < 0) {
+            r.containerRadius = 0
+        }
+        if (r.trigger < 0) {
+            r.trigger = 0
+        }
+        if (r.hideUnderWidth < 0) {
+            r.hideUnderWidth = 0
+        }
+        if (r.title === "") {
+            r.titleAsText = false
+        }
+        var u = {};
+        u = {
+            position: "fixed",
+            width: r.containerSize + 18,
+            height: r.containerSize,
+            cursor: "pointer"
+        };
+        u["bottom"] = r.bottomOffset;
+        u[r.location] = r.locationOffset;
+        u["border-radius"] = r.containerRadius;
+        e(i).css(u);
+        if (!r.titleAsText) {
+            e(i).attr("title", r.title)
+        } else {
+            e("body").append('<div class="' + r.titleAsTextClass + '">' + r.title + "</div>");
+            var a = e("." + r.titleAsTextClass);
+            e(a).attr("style", e(i).attr("style"));
+            e(a).css("background", "transparent").css("width", r.containerSize + 40).css("height", "auto").css("text-align", "center").css(r.location, r.locationOffset - 20);
+            var f = e(a).height() + 10;
+            e(i).css("bottom", "+=" + f + "px")
+        }
+        var h = false;
+        e(window).resize(function() {
+            if (e(window).outerWidth() <= r.hideUnderWidth) {
+                h = true;
+                t(e(i), "hide", r.entryAnimation);
+                if (a)
+                    t(e(a), "hide", r.entryAnimation)
+            } else {
+                h = false;
+                e(window).trigger("scroll")
+            }
+        });
+        if (e(window).outerWidth() <= r.hideUnderWidth) {
+            h = true;
+            e(i).hide();
+            if (a)
+                e(a).hide()
+        }
+        if (!r.alwaysVisible) {
+            e(window).scroll(function() {
+                if (e(window).scrollTop() >= r.trigger && !h) {
+                    t(e(i), "show", r.entryAnimation);
+                    if (a)
+                        t(e(a), "show", r.entryAnimation)
+                }
+                if (e(window).scrollTop() < r.trigger && !h) {
+                    t(e(i), "hide", r.entryAnimation);
+                    if (a)
+                        t(e(a), "hide", r.entryAnimation)
+                }
+            })
+        } else {
+            t(e(i), "show", r.entryAnimation);
+            if (a)
+                t(e(a), "show", r.entryAnimation)
+        }
+        if (e(window).scrollTop() >= r.trigger && !h) {
+            t(e(i), "show", r.entryAnimation);
+            if (a)
+                t(e(a), "show", r.entryAnimation)
+        }
+        var p = true;
+        e(i).add(a).on("click", function() {
+            if (p) {
+                p = false;
+                e("html,body").animate({
+                    scrollTop: 0
+                }, r.goupSpeed, function() {
+                    p = true
+                })
+            }
+            return false
+        })
+    }
+})(jQuery)
